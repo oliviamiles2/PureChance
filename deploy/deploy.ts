c9e01ba08f20591d28e4de26358ab7623ab20208
oliviamiles2 <xlcihu35504@outlook.com>
2025-12-25 16:05:09 +0800

    Deploy PureChance in deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,16 +2,20 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hre.network.name === "sepolia" && (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "")) {
+    throw new Error("PRIVATE_KEY is required in .env to deploy on sepolia");
+  }
+
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedPureChance = await deploy("PureChance", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`PureChance contract: `, deployedPureChance.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_pureChance"; // id required to prevent reexecution
+func.tags = ["PureChance"];
